8fcfb0e3b369f1fc2042c95e792dcab803ed616f
bartek14aa <62958388+bartek14aa@users.noreply.github.com>
2022-06-14 17:00:00 +0200

    Add files via upload

---
+++ michalak_bartlomiej_mapa/data/Pasystartoweidrogikoowaniasamolotw_12.js
@@ -0,0 +1,11 @@
+var json_Pasystartoweidrogikoowaniasamolotw_12 = {
+"type": "FeatureCollection",
+"name": "Pasystartoweidrogikoowaniasamolotw_12",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "full_id": "w396591378", "osm_id": "396591378", "osm_type": "way", "aeroway": "runway", "ele": "94", "icao": "EPLS", "length": "1320", "ref": "05R\/23L", "surface": "grass", "width": "50", "wikidata": "Q11762727", "wikipedia": "pl:Lotnisko Leszno-Strzyżewice" }, "geometry": { "type": "LineString", "coordinates": [ [ 16.5286136, 51.8358164 ], [ 16.5233408, 51.8336351 ], [ 16.5141713, 51.829747 ] ] } },
+{ "type": "Feature", "properties": { "fid": "2", "full_id": "w692814004", "osm_id": "692814004", "osm_type": "way", "aeroway": "taxiway", "ele": "", "icao": "", "length": "", "ref": "B1", "surface": "grass_paver", "width": "", "wikidata": "", "wikipedia": "" }, "geometry": { "type": "LineString", "coordinates": [ [ 16.5311277, 51.8321726 ], [ 16.5308939, 51.8327187 ], [ 16.53075, 51.8330756 ], [ 16.5298732, 51.8351708 ], [ 16.5289207, 51.8358859 ], [ 16.5287637, 51.8358637 ], [ 16.5286793, 51.83584 ], [ 16.5286136, 51.8358164 ] ] } },
+{ "type": "Feature", "properties": { "fid": "3", "full_id": "w692814005", "osm_id": "692814005", "osm_type": "way", "aeroway": "taxiway", "ele": "", "icao": "", "length": "", "ref": "A", "surface": "grass", "width": "", "wikidata": "", "wikipedia": "" }, "geometry": { "type": "LineString", "coordinates": [ [ 16.53075, 51.8330756 ], [ 16.5244322, 51.8320754 ], [ 16.5203885, 51.8314352 ] ] } },
+{ "type": "Feature", "properties": { "fid": "4", "full_id": "w784006460", "osm_id": "784006460", "osm_type": "way", "aeroway": "taxiway", "ele": "", "icao": "", "length": "", "ref": "B2", "surface": "grass_paver", "width": "", "wikidata": "", "wikipedia": "" }, "geometry": { "type": "LineString", "coordinates": [ [ 16.5289207, 51.8358859 ], [ 16.5275039, 51.836861 ], [ 16.5274755, 51.8368796 ], [ 16.5274406, 51.8368927 ], [ 16.5274008, 51.8368978 ], [ 16.5273604, 51.8368944 ], [ 16.5273242, 51.8368828 ], [ 16.5271925, 51.8368233 ] ] } }
+]
+}
